34128bd7dab154a36c5cfb511ae6f6e34af0e02c
Dhanusri8 <dhanusritalla@gmail.com>
2026-06-29 23:41:35 +0530

    Removed spaCy dependency

--- scoring.py
+++ scoring.py
@@ -1,9 +1,7 @@
 import re
-import spacy
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
-nlp = spacy.load("en_core_web_sm")
 
 def clean_text(text):
     text = text.lower()
@@ -11,26 +9,37 @@ def clean_text(text):
     text = re.sub(r'\s+', ' ', text)
     return text
 
+
 def calculate_similarity(resume, job_desc):
     vectorizer = TfidfVectorizer()
     vectors = vectorizer.fit_transform([resume, job_desc])
     similarity = cosine_similarity(vectors[0:1], vectors[1:2])
     return round(similarity[0][0] * 100, 2)
 
+
 def extract_skills(text):
     skills_db = [
-        "python", "sql", "machine learning", "data structures",
-        "html", "css", "c", "numpy", "pandas", "scikit learn"
+        "python",
+        "sql",
+        "machine learning",
+        "data structures",
+        "html",
+        "css",
+        "c",
+        "numpy",
+        "pandas",
+        "scikit learn"
     ]
 
-    doc = nlp(text)
+    text = text.lower()
     found = []
 
-    for token in doc:
-        if token.text.lower() in skills_db:
-            found.append(token.text.lower())
+    for skill in skills_db:
+        if skill in text:
+            found.append(skill)
 
     return list(set(found))
+
 
 def skill_match(resume, job_desc):
     resume_skills = set(extract_skills(resume))
@@ -40,6 +49,7 @@ def skill_match(resume, job_desc):
     missing = job_skills - resume_skills
 
     return list(matched), list(missing)
+
 
 def advanced_score(similarity, matched, total_skills):
     if total_skills == 0:
